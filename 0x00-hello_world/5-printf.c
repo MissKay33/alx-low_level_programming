c9f80bd1663aafd01f1750cb7f721a414ff95ddb
MissKay33 <karabosasha@googlemail.com>
2023-08-14 08:35:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include<studio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed by a new line
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line
  * Return: Always 0.
  *
  */
